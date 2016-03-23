235e766b26106f7f96a2586a6c570cb9c8b39e17
sibtain_usa <superali_usa@yahoo.com>
2016-03-23 19:53:32 -0400

    refactored navbar into a custom directive

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,9 @@
 angular.module('learnNg', []);
+
+
+angular.module('learnNg')
+        .directive('navBar', function(){
+          return {
+            templateUrl: 'html/templates/nav.html',
+          };
+        });
